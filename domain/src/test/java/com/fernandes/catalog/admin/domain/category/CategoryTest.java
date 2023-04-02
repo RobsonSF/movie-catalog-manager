fb126e8da9ca69c742e543578e6d8e4081032034
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-01 23:40:50 -0300

    test: if Category name null then return an Exception

--- domain/src/test/java/com/fernandes/catalog/admin/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fernandes/catalog/admin/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.fernandes.catalog.admin.domain.category;
 
+import com.fernandes.catalog.admin.domain.exceptions.DomainException;
+import com.fernandes.catalog.admin.domain.validation.handler.TrowsValidationHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,7 +14,8 @@ public class CategoryTest {
         final var expectedDescription = "most watched";
         final var expectedIsActive = true;
 
-        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
         assertNotNull(actualCategory);
         assertNotNull(actualCategory.getId());
@@ -22,5 +25,22 @@ public class CategoryTest {
         assertNotNull(actualCategory.getCreatedAt());
         assertNotNull(actualCategory.getUpdatedAt());
         assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAnInvalidParam_WhenCallNewCategoryAndValidate_ThenReturnException(){
+        final String expectedName = null;
+        final var expectedDescription = "most watched";
+        final var expectedIsActive = true;
+        final var  expectedErrorMessage = "'name' should not be null";
+        final var  expectedErrorCount = 1;
+
+        final var actualCategory =
+                Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException =
+                assertThrows(DomainException.class, () -> actualCategory.validate(new TrowsValidationHandler()));
+
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
     }
 }
