ff68197d813ddeb86ed71ab4960bec3805ac9999
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-22 20:23:18 -0300

    feat: abstract UseCase created

--- application/src/main/java/com/fernandes/catalog/admin/application/Main.java
+++
@@ -1,7 +0,0 @@
-package com.fernandes.catalog.admin.application;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ application/src/main/java/com/fernandes/catalog/admin/application/UseCase.java
@@ -0,0 +1,5 @@
+package com.fernandes.catalog.admin.application;
+
+public abstract class UseCase<IN, OUT> {
+    public abstract OUT execute(IN anIn);
+}
