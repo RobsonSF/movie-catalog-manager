c0d141ce1440b382fe17a2923c6dce39f9d4aa9f
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-22 20:20:36 -0300

    chore: rename to ThrowsValidationHandler

--- domain/src/main/java/com/fernandes/catalog/admin/domain/validation/handler/ThrowsValidationHandler.java
+++ domain/src/main/java/com/fernandes/catalog/admin/domain/validation/handler/ThrowsValidationHandler.java
@@ -6,7 +6,7 @@ import com.fernandes.catalog.admin.domain.validation.ValidationHandler;
 
 import java.util.List;
 
-public class TrowsValidationHandler implements ValidationHandler {
+public class ThrowsValidationHandler implements ValidationHandler {
     @Override
     public ValidationHandler append(final Error anError) {
         throw DomainException.with(anError);
