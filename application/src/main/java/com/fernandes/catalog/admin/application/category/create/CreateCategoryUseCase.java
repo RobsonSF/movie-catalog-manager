7dfb2a539b5f2b193a69ccbd020d0eff7aa7f824
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-22 20:34:57 -0300

    feat: abstract CreateCategoryUseCase created

---
+++ application/src/main/java/com/fernandes/catalog/admin/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,7 @@
+package com.fernandes.catalog.admin.application.category.create;
+
+import com.fernandes.catalog.admin.application.UseCase;
+
+public abstract class CreateCategoryUseCase
+        extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+}
