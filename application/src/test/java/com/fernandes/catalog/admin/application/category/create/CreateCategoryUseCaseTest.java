8604aa414689cc28e6cc22d45939aa18890e1347
Robson S Fernandes <rbsn.dev@gmail.com>
2023-04-22 21:13:47 -0300

    test: CreateCategoryUseCaseTest if empty name will throw DomainException

--- application/src/test/java/com/fernandes/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fernandes/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,32 +1,43 @@
 package com.fernandes.catalog.admin.application.category.create;
 
 import com.fernandes.catalog.admin.domain.category.CategoryGateway;
+import com.fernandes.catalog.admin.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase defaultCreateCategoryUseCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
 
     public static final String EXPECTED_NAME = "movies";
     public static final String EXPECTED_DESCRIPTION = "most watched";
     public static final boolean EXPECTED_IS_ACTIVE = true;
 
     @Test
-    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId(){
+    public void givenAValidCommand_whenCallsCreateCategory_thenShouldReturnCategoryId(){
 
         final var  aCommand = CreateCategoryCommand.with(EXPECTED_NAME, EXPECTED_DESCRIPTION, EXPECTED_IS_ACTIVE);
 
-        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
         when(categoryGateway.create(any())).thenAnswer(returnsFirstArg());
 
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
-        final var actualOutput = useCase.execute(aCommand);
+        final var actualOutput = defaultCreateCategoryUseCase.execute(aCommand);
 
         assertNotNull(actualOutput);
         assertNotNull(actualOutput.id());
@@ -40,5 +51,20 @@ public class CreateCategoryUseCaseTest {
                     && Objects.nonNull(aCategory.getUpdatedAt())
                     && Objects.isNull(aCategory.getDeletedAt());
         }));
+    }
+
+    @Test
+    public void givenAInvalidEmptyName_whenCallsCreateCategory_thenShouldReturnDomainException(){
+        final var invalidName = "";
+        final var expectedErrorMassage = "'name' should not be empty";
+
+        final var  aCommand = CreateCategoryCommand.with(invalidName, EXPECTED_DESCRIPTION, EXPECTED_IS_ACTIVE);
+
+        final var actualException =
+                assertThrows(DomainException.class, () -> defaultCreateCategoryUseCase.execute(aCommand));
+
+        assertEquals(expectedErrorMassage, actualException.getMessage());
+
+        verify(categoryGateway, times(0)).create(any());
     }
 }
